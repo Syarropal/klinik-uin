c95766fb53c24e09002aafd1a8dff46d29ae55fe
Syarropal <syarropalharpi3@gmail.comm>
2026-06-20 03:26:56 +0700

    Update auto-create user to use username

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Mengecek apakah tabel users sudah ada
+        if (Schema::hasTable('users')) {
+            // Mengecek apakah username 'syarropal' sudah ada
+            if (!User::where('username', 'syarropal')->exists()) {
+                User::create([
+                    'nama_lengkap' => 'Syarropal',
+                    'username' => 'syarropal', // Ini yang digunakan untuk login
+                    'role' => 'admin',
+                    'password' => Hash::make('12345'), // Password disamakan dengan data lama
+                ]);
+            }
+        }
     }
 }
